c27fcc960fcc5cf2bd0febc69d7dd5197453f0e7
Sanket Patel <patelsanket864@gmail.com>
2020-04-16 10:56:51 +0530

    code cleanup

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet,Text, View, TextInput, Button, FlatList } from 'react-native';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
